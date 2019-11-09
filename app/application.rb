97301923bc97c0bd1bbf3be712c9126d8ea979e7
Christian Perez <perezchristian1012@gmail.com>
2019-11-09 04:44:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,5 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path.match(/item/)
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
   end
 end
