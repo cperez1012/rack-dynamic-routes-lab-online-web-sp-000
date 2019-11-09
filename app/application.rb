e03b91669eb075eb9433fa18b8c26970da3257c0
Christian Perez <perezchristian1012@gmail.com>
2019-11-09 04:54:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find {|item| item.name == item_name }
-      if
+      if item
         resp.write(item.price)
       else
         resp.write("Item not found.")
